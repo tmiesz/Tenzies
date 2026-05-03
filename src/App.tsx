3f9d52871543eab898d6cfc7598fd74dacc083ae
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-05-03 22:55:30 +0200

    style fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import Die from "./components/Die";
 export default function App() {
   return (
     <>
-      <div className="flex flex-col items-center">
+      <div className="border flex flex-col items-center">
         <h1>Tenzies</h1>
         <span>
           Roll until all dice are the same. Click each die to freeze it at
           current value between rolls.
         </span>
       </div>
-      <div className="grid grid-cols-6 gap-4 p-4">
-        {Array.from({ length: 12 }).map((_, i) => (
-          <Die
-            key={i}
-            id={i}
-            value="1"
-            onClick={() => console.log(`LOCKING ${i}`)}
-          />
-        ))}
+      <div className="border w-3xl m-auto">
+        <div className="grid grid-cols-6 gap-4 p-4">
+          {Array.from({ length: 12 }).map((_, i) => (
+            <Die
+              key={i}
+              id={i}
+              value="1"
+              onClick={() => console.log(`LOCKING ${i}`)}
+            />
+          ))}
+        </div>
       </div>
       <div className="flex flex-col items-center">
         <button className="border">Roll</button>
