92122f9ab93d93f7e7b47011f2cd2011fb482453
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-05-03 12:43:09 +0200

    some text

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <div>
+        <h1>Tenzies</h1>
+        <span>
+          Roll until all dice are the same. Click each die to freeze it at
+          current value between rolls.
+        </span>
+      </div>
+    </>
+  );
 }
