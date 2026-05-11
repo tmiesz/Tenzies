e6a56b2f5a7e8e05023b31a3412a13734ba56258
Tomasz Mieszkowicz <tommieszkowicz@gmail.com>
2026-05-11 21:20:51 +0200

    add initial holddice funcitonality

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,51 @@ import Die, { type DieProps } from "./components/Die";
 import { v4 as uuidv4 } from "uuid";
 
 export default function App() {
-  const [board, setBoard] = useState(GenerateDice());
+  const [board, setBoard] = useState(generateDice());
 
-  function GenerateDice(): DieProps[] {
+  function generateDice(): DieProps[] {
     return Array(10)
       .fill(0)
       .map(() => ({
         id: uuidv4(),
         value: Math.ceil(Math.random() * 6),
         locked: false,
+        hold: holdDie,
       }));
   }
 
-  const dice = board.map((props) => (
-    <Die id={props.id} value={props.value} locked={props.locked} />
+  const dice = board.map((die) => (
+    <Die
+      id={die.id}
+      key={die.id}
+      value={die.value}
+      locked={die.locked}
+      hold={die.hold}
+    />
   ));
 
   function rollDice() {
-    setBoard(GenerateDice());
+    setBoard(generateDice());
+  }
+
+  function holdDie(id: string) {
+    console.log(id);
   }
 
   return (
     <>
       <div className="border flex flex-col gap-4 items-center">
-        <div className="border">
-          <h1 className="var(--accent)">Tenzies</h1>
+        <div className="border flex flex-col items-center p-4">
+          <h1 className="var(--accent) text-3xl">Tenzies</h1>
           <span>
             Roll until all dice are the same. Click each die to freeze it at
             current value between rolls.
           </span>
         </div>
-        <div className="border grid grid-cols-5 grid-rows-2 w-2xl">{dice}</div>
+        <div className="border grid grid-cols-5 w-2xl gap-2">{dice}</div>
         <div className="border">
           <button
-            className="bg-(--foreground) rounded-2xl text-(--accent) text-4xl p-4"
+            className="bg-(--foreground) rounded-2xl text-(--accent) text-4xl p-4 cursor-pointer"
             onClick={rollDice}
           >
             Roll
--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -2,12 +2,14 @@ export type DieProps = {
   id: string;
   value: number;
   locked: boolean;
+  hold: (id: string) => void;
 };
 
-export default function Die({ value, locked }: DieProps) {
+export default function Die({ id, value, locked, hold }: DieProps) {
   return (
     <button
-      className={`bg-(--foreground) border text-(--accent) rounded-4xl aspect-square text-5xl flex items-center justify-center ${locked ? "border-(--accent)" : "border-(--secondary)"}`}
+      className={`bg-(--foreground) border text-(--accent) rounded-4xl aspect-square text-5xl flex items-center justify-center cursor-pointer ${locked ? "border-(--accent)" : "border-(--secondary)"}`}
+      onClick={() => hold(id)}
     >
       {value}
     </button>
